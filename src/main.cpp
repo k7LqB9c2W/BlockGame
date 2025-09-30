a48154e50dc5181199757a71bf70fbb786a14f12
Jason Ricketts <jacobricketts2005@gmail.com>
2025-09-29 23:50:52 -0700

    moved things to new dedicated file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include "TextureLoader.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -35,8 +36,6 @@
 #include <deque>
 #include <map>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
 namespace
 {
@@ -327,65 +326,6 @@ void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 
     return program;
 }
-
-struct LoadedTexture
-{
-    GLuint id{0};
-    glm::ivec2 size{0};
-};
-
-[[nodiscard]] LoadedTexture loadTexture(const char* path)
-{
-    LoadedTexture texture{};
-    glGenTextures(1, &texture.id);
-
-    int width = 0;
-    int height = 0;
-    int channels = 0;
-    stbi_set_flip_vertically_on_load(false);
-    unsigned char* data = stbi_load(path, &width, &height, &channels, 0);
-
-    if (!data)
-    {
-        std::cerr << "Failed to load texture: " << path << std::endl;
-        glDeleteTextures(1, &texture.id);
-        texture.id = 0;
-        texture.size = glm::ivec2(0);
-        return texture;
-    }
-
-    GLenum format = GL_RGB;
-    if (channels == 1)
-    {
-        format = GL_RED;
-    }
-    else if (channels == 3)
-    {
-        format = GL_RGB;
-    }
-    else if (channels == 4)
-    {
-        format = GL_RGBA;
-    }
-
-    glBindTexture(GL_TEXTURE_2D, texture.id);
-    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    stbi_image_free(data);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    texture.size = glm::ivec2(width, height);
-    std::cout << "Loaded texture: " << path << " (" << texture.size.x << "x" << texture.size.y << ")" << std::endl;
-
-    return texture;
-}
-
 class Crosshair
 {
 public:

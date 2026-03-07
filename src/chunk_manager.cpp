51bdf5a2019bf98fa9aeba98217a6bfac465b8c0
Jacob Ricketts <193657779+k7LqB9c2W@users.noreply.github.com>
2026-03-06 22:25:09 -0800

    added a lot of fixes for terrain

--- src/chunk_manager.cpp
+++ src/chunk_manager.cpp
@@ -1102,7 +1102,7 @@ void ChunkManager::Impl::setBlockTextureAtlasConfig(const glm::ivec2& textureSiz
 
     for (BlockFace face : {BlockFace::Top, BlockFace::Bottom, BlockFace::North, BlockFace::South, BlockFace::East, BlockFace::West})
     {
-        assignFace(BlockId::Stone, face, {0, 9});
+        assignFace(BlockId::Stone, face, {0, 8});
     }
 
     blockAtlasConfigured_ = true;
